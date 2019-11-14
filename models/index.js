7de6731ca9c62d1595a7c5efff81c6591c156817
Rachel Burrous <rachelashleyburrous@gmail.com>
2019-11-14 16:10:01 -0500

    wikistackv3

--- models/index.js
+++ models/index.js
@@ -1,19 +1,21 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', 
+logging: false
+});
 
-module.exports = {
-  db
-}
+// module.exports = {
+//   db
+// }
 
 const Page = db.define('page', {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING, allowNull: false
   },
   slug: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING, allowNull: false
   },
   content: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT, allowNull: false
   },
   status: {
     type: Sequelize.ENUM('open', 'closed')
@@ -22,11 +24,11 @@ const Page = db.define('page', {
 
 const User = db.define('user', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING, allowNull: false
   },
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING, allowNull: false
   }
 });
 
-module.exports = { Page, User };
+module.exports = { db, Page, User };
