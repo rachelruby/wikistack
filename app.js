c6c567ae9c63d30fef297834a2a61be677af7e48
Rachel Burrous <rachelashleyburrous@gmail.com>
2019-11-14 13:18:02 -0500

    start

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
